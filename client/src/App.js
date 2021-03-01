2cb067373268cac0ed50aa497501eb3f13cb49e8
FaziletKosure <faziletkosure1@gmail.com>
2021-03-01 23:47:41 +0100

    Created components and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React,{useState} from 'react'
 import './App.css';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+import {UserContext} from './UserContext'
+import Chat from './components/chat/Chat'
+import Home from './components/home/Home'
+
 
 function App() {
+  const [user,setUser]=useState(null)
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <UserContext.Provider value={{user,setUser}}>
+      <Switch>
+       <Route exact path='/' component={Home}/>
+       <Route exact path='/chat' component={Chat}/>
+      </Switch>
+    </UserContext.Provider>
     </div>
+    </Router>
   );
 }
 
