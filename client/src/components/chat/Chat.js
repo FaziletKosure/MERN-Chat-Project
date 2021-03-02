e99f5e42dd1f7f3dc340e6574f2355af2d08ab3f
FaziletKosure <faziletkosure1@gmail.com>
2021-03-02 18:03:54 +0100

    Created userContext

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React,{useContext} from 'react'
+
+import {Link} from 'react-router-dom'
+
+import {UserContext} from '../../UserContext'
 
 const Chat = () => {
+    const {user,setUser}=useContext(UserContext)
     return (
         <div>
-            <h1>Chat</h1>
+            <h1>Chat {JSON.stringify(user)}</h1> 
+            <Link to={'/'}>
+               <button>go to home</button>
+           </Link>
             
         </div>
     )
--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React,{useContext} from 'react'
+
+import {Link} from 'react-router-dom'
+
+import {UserContext} from '../../UserContext'
 
 const Home = () => {
+
+    const {user,setUser}=useContext(UserContext)
+
+    const setAsJohn=()=>{
+        const john={
+            name:'John',
+            email:'john@email.com',
+            password:'123',
+            id:'123'
+        }
+        setUser(john)
+    }
+
+    const setAsTom=()=>{
+        const tom={
+            name:'Tom',
+            email:'tom@email.com',
+            password:'456',
+            id:'456'
+        }
+        setUser(tom)
+    }
     return (
         <div>
-           <h1>Home</h1> 
+           <h1>Home {JSON.stringify(user)}</h1> 
+           <button onClick={setAsJohn}>set as John</button>
+           <button onClick={setAsTom}>set as Tom</button>
+           <Link to={'/chat'}>
+               <button>go to chat</button>
+           </Link>
         </div>
     )
 }
