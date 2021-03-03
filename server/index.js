fde8b5e53d727a98bc91ee55712b15107cb139fc
FaziletKosure <faziletkosure1@gmail.com>
2021-03-03 22:03:44 +0100

    received the room name

--- server/index.js
+++ server/index.js
@@ -1,14 +1,24 @@
 const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const http = require('http').createServer(app);
+const socketio=require('socket.io')
+const cors = require('cors')
+const io = socketio(http)
+// const io = require("socket.io")(http, {
+//     origins: ["http://localhost:3000"]
+//   });
+
 const PORT= process.env.PORT || 5000
+app.use(cors())
 
 // app.get('/', (req, res) => {
 //   res.sendFile(__dirname + '/index.html');
 // });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log(socket.id);
+  socket.on('create-room',name=>{
+      console.log('Then room name received is ',name);
+  })
 });
 
 http.listen( PORT, () => {
---
+++ server/models/Room.js
@@ -0,0 +1,11 @@
+const mongoose=require('mongoose')
+
+const roomSchema= new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    }
+})
+
+const Room=mongoose.model('room',roomSchema)
+module.exports=Room
