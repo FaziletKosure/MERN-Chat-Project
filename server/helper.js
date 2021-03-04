ed8da679acb5788dab76b7ebd4b0b6c03bb26f7d
FaziletKosure <faziletkosure1@gmail.com>
2021-03-04 19:39:15 +0100

    created other  helper functions

--- server/helper.js
+++ server/helper.js
@@ -17,4 +17,12 @@ const addUser=({socket_id,name,user_id,room_id})=>{
 
 }
 
-module.exports={addUser}
+const removeUser = (socket_id) => {
+    const index = users.findIndex(user => user.socket_id === socket_id);
+    if (index !== -1) {
+        return users.splice(index, 1)[0]
+    }
+}
+const getUser = (socket_id) => users.find(user => user.socket_id === socket_id)
+
+module.exports={addUser,removeUser,getUser}
